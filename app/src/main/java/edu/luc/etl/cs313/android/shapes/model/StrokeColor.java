ec9c73c85ad7cc8717464cdae670677f72a93e0a
Dean Ahmeti <deanahmeti11@gmail.com>
2026-03-22 20:09:43 -0500

    worked on StrokeColor.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
@@ -7,20 +7,24 @@ package edu.luc.etl.cs313.android.shapes.model;
 public final class StrokeColor implements Shape {
 
     // TODO entirely your job
+    private final int color; //store color value
+    private final Shape shape; //underlying shape will pair with a stroke color.
 
     public StrokeColor(final int color, final Shape shape) {
+        this.color = color; //initialize stroke color
+        this.shape = shape; //initialize underlyig shape
     }
 
     public int getColor() {
-        return -1;
+        return color;
     }
 
     public Shape getShape() {
-        return null;
+        return shape;
     }
 
     @Override
     public <Result> Result accept(Visitor<Result> v) {
-        return null;
+        return shape.accept(v); //ensures visitor will work on shape being colored.
     }
 }
