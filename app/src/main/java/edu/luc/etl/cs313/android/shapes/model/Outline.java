06e634c044a13844e7f825f7e1c952ec55cd7a77
Dean Ahmeti <deanahmeti11@gmail.com>
2026-03-22 19:49:09 -0500

    worked on outline.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Outline.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Outline.java
@@ -18,7 +18,7 @@ public final class Outline implements Shape {
 
     @Override
     public <Result> Result accept(final Visitor<Result> v) {
-        // TODO your job
-        return null;
+        // TODO your job -Pair 09 completed
+        return shape.accept(v); //ensure visitor works on outlined shape
     }
 }
